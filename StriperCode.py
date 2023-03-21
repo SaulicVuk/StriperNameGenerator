9d880ffc4feeb85cefba3c299d8193a62d4b49b7
SaulicVuk <saulicvuk1@gmail.com>
2023-03-21 10:49:06 +0100

    Initial commit

---
+++ StriperCode.py
@@ -0,0 +1,79 @@
+month = input("enter the month you were born: ")
+date = input("enter the day you were born: ")
+
+monthConversions = {
+    "January": "Bloom",
+    "February": "Boulder",
+    "March": "Bailey",
+    "April": "Noble",
+    "May": "Colt",
+    "June": "Potter",
+    "July": "Kelly",
+    "August": "Poe",
+    "September": "Lily",
+    "October": "Underwood",
+    "November": "Paris",
+    "December": "Ivy"
+}
+if ( date == "1"):
+    print( monthConversions.get(month) + " Tiffany")
+elif (date == "2"):
+    print( monthConversions.get(month) + " Amber")
+elif (date == "3"):
+    print( monthConversions.get(month) + " Brandy")
+elif (date == "4"):
+    print(monthConversions.get(month) + " Lola")
+elif (date == "5"):
+    print(monthConversions.get(month) + " Angel")
+elif (date == "6"):
+    print(monthConversions.get(month) + " Ginger")
+elif (date == "7"):
+    print(monthConversions.get(month) + " Candy")
+elif (date == "8"):
+    print(monthConversions.get(month) + " Hope")
+elif (date == "9"):
+    print(monthConversions.get(month) + " Anastasia")
+elif (date == "10"):
+    print(monthConversions.get(month) + " Cherry")
+elif (date == "11"):
+    print(monthConversions.get(month) + " Kitty")
+elif (date == "12"):
+    print(monthConversions.get(month) + " Gunn")
+elif (date == "13"):
+    print(monthConversions.get(month) + " Abner")
+elif (date == "14"):
+    print(monthConversions.get(month) + " Walker")
+elif (date == "15"):
+    print(monthConversions.get(month) + " Charity")
+elif (date == "16"):
+    print(monthConversions.get(month) + " Cadwell")
+elif (date == "17"):
+    print(monthConversions.get(month) + " Campbell")
+elif (date == "18"):
+    print(monthConversions.get(month) + " Vow")
+elif (date == "19"):
+    print(monthConversions.get(month) + " Hunter")
+elif (date == "20"):
+    print(monthConversions.get(month) + " Abernathy")
+elif (date == "21"):
+    print(monthConversions.get(month) + " Ice")
+elif (date == "22"):
+    print(monthConversions.get(month) + " Blackwood")
+elif (date == "23"):
+    print(monthConversions.get(month) + " Ash")
+elif (date == "24"):
+    print(monthConversions.get(month) + " Holly")
+elif (date == "25"):
+    print(monthConversions.get(month) + " Rose")
+elif (date == "26"):
+    print(monthConversions.get(month) + " Dalton")
+elif (date == "27"):
+    print(monthConversions.get(month) + " Annesley")
+elif (date == "28"):
+    print(monthConversions.get(month) + " Archer")
+elif (date == "29"):
+    print(monthConversions.get(month) + " Pevensie")
+elif (date == "30"):
+    print(monthConversions.get(month) + " Violet")
+elif (date == "31"):
+    print(monthConversions.get(month) + " East")
